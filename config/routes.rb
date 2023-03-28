992da6b47eb177c514c5869f71f641d641378d2c
SekoViper <damehlivingstone1@gmail.com>
2023-03-28 13:21:29 +0000

    Add routing for pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :recipe_foods
+  resources :recipes
+  resources :foods
+  resources :users
+
+  root 'foods#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
